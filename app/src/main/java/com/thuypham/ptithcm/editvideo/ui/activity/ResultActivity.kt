48a556e41ea6f3e517aad09dcddf9ae71ad6dea9
archko <archko@gmail.com>
2022-04-13 19:26:36 +0800

    添加播放的方向感应. 播放到另一个页面,避免原来的页面数据消失了

---
+++ app/src/main/java/com/thuypham/ptithcm/editvideo/ui/activity/ResultActivity.kt
@@ -0,0 +1,63 @@
+package com.thuypham.ptithcm.editvideo.ui.activity
+
+import android.annotation.TargetApi
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import com.thuypham.ptithcm.editvideo.R
+import com.thuypham.ptithcm.editvideo.base.BaseActivity
+import com.thuypham.ptithcm.editvideo.databinding.ActivityMainBinding
+import com.thuypham.ptithcm.editvideo.ui.fragment.home.HomeFragment.Companion.RESULT_PATH
+import com.thuypham.ptithcm.editvideo.ui.fragment.result.ResultFragment
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (intent == null || TextUtils.isEmpty(intent.getStringExtra(RESULT_PATH))) {
+            finish()
+            return
+        }
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val resultFragment = ResultFragment()
+        resultFragment.arguments = bundleOf(
+            RESULT_PATH to intent.getStringExtra(RESULT_PATH)
+        )
+        supportFragmentManager
+            .beginTransaction()
+            .add(
+                android.R.id.content,
+                resultFragment
+            ).commit()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+        }
+    }
+}
