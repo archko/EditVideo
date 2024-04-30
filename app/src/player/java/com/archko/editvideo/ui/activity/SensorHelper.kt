099f9b06960e7970a7595df8b0ed4f6bdd3fea8e
archko <archko@gmail.com>
2024-04-30 16:27:13 +0800

    升级到media3 播放器修改

---
+++ app/src/player/java/com/archko/editvideo/ui/activity/SensorHelper.kt
@@ -0,0 +1,23 @@
+package com.archko.editvideo.ui.activity
+
+import android.content.pm.ActivityInfo
+import androidx.activity.ComponentActivity
+
+/**
+ * @author: archko 2018/7/22 :13:03
+ */
+class SensorHelper(private val activity: ComponentActivity) {
+
+    private var prevOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    fun setOrientation(orientation: Int) {
+        if (orientation != prevOrientation) {
+            activity.requestedOrientation = orientation
+            prevOrientation = orientation
+        }
+    }
+
+    fun getOrientation(): Int {
+        return activity.requestedOrientation
+    }
+
+}
