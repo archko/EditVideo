763f4bef86131b80f3bba0360b45da1c78ad17ec
archko <archko@gmail.com>
2024-08-19 11:06:25 +0800

    处理api34的权限 问题

--- app/src/main/java/com/thuypham/ptithcm/editvideo/ui/activity/MainActivity.kt
+++ app/src/main/java/com/thuypham/ptithcm/editvideo/ui/activity/MainActivity.kt
@@ -1,126 +1,157 @@
 package com.thuypham.ptithcm.editvideo.ui.activity
 
 import android.Manifest
-import android.app.Activity
 import android.app.AlertDialog
-import android.content.Context
-import android.content.DialogInterface
+import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.Settings
 import android.util.Log
 import android.view.View
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.thuypham.ptithcm.editvideo.R
 import com.thuypham.ptithcm.editvideo.base.BaseActivity
 import com.thuypham.ptithcm.editvideo.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main),
+    OnPermissionGranted {
 
-    private val REQUEST_PERMISSION_CODE = 0x01
     var view: View? = null
+    private val permissionCallbacks = arrayOfNulls<OnPermissionGranted>(PERMISSION_LENGTH)
+    private var permissionDialog: Dialog? = null
 
     override fun setupView() {
         view = findViewById(R.id.container)
-        checkSdcardPermission()
+        checkForExternalPermission()
     }
 
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { result: ActivityResult ->
-            if (result.resultCode == RESULT_OK) {
-                runOnUiThread {
-                    if (view != null) {
-                        /*val snackBar = Snackbar.make(
-                            view ?: return@Runnable,
-                            getString(R.string.external_permission_denied), Snackbar.LENGTH_SHORT
-                        )
-                        snackBar.show()*/
-                        showDialog(
-                            this,
-                            "\"本应用需要获取\"访问所有文件\"权限，请给予此权限，否则无法使用本应用\""
-                        ) { _: DialogInterface?, _: Int ->
-                            requestSdcardPermission()
-                        }
+    private fun checkForExternalPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!checkStoragePermission()) {
+                requestStoragePermission(this, true)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                requestAllFilesAccess(this)
+            }
+        } else {
+            loadView()
+        }
+    }
+
+    private fun loadView() {
+
+    }
+
+    private fun checkStoragePermission(): Boolean {
+        return (ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+                == PackageManager.PERMISSION_GRANTED)
+    }
+
+    open fun requestStoragePermission(
+        onPermissionGranted: OnPermissionGranted, isInitialStart: Boolean
+    ) {
+        val permission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        permissionCallbacks[STORAGE_PERMISSION] = onPermissionGranted
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("获取权限")
+                .setMessage("\"本应用需要获取\"访问所有文件\"权限，请给予此权限，否则无法使用本应用\"")
+                .setPositiveButton("取消") { _, _ ->
+                    finish()
+                }
+                .setNegativeButton("确定") { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        this, arrayOf(permission), STORAGE_PERMISSION
+                    )
+                    permissionDialog?.run {
+                        permissionDialog!!.dismiss()
                     }
                 }
-            } else {
-                requestSdcardPermission()
-            }
-        }
-
-    private fun hasStoragePermission(context: Context): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (Environment.isExternalStorageManager()) {
-                Log.i("ABCD", "此手机是Android 11或更高的版本，且已获得访问所有文件权限")
-                return true
-            } else {
-                Log.i("ABCD", "此手机是Android 11或更高的版本，且没有访问所有文件权限")
-            }
-            false
-        } else {
-            Log.i(
-                "ABCD",
-                "此手机版本小于Android 11，版本为：API \${Build.VERSION.SDK_INT}，不需要申请文件管理权限"
-            )
-            ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) === PackageManager.PERMISSION_GRANTED
+            builder.setCancelable(false)
+            builder.create().show()
+        } else if (isInitialStart) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), STORAGE_PERMISSION)
         }
     }
 
-    private fun checkSdcardPermission() {
-        if (!hasStoragePermission(this)) {
-            requestSdcardPermission()
+    open fun requestAllFilesAccess(onPermissionGranted: OnPermissionGranted) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("获取权限")
+                .setMessage("\"本应用需要获取\"访问所有文件\"权限，请给予此权限，否则无法使用本应用\"")
+                .setPositiveButton("取消") { _, _ ->
+                    finish()
+                }
+                .setNegativeButton("确定") { _, _ ->
+                    permissionCallbacks[ALL_FILES_PERMISSION] = onPermissionGranted
+                    try {
+                        val intent =
+                            Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                                .setData(Uri.parse("package:$packageName"))
+                        startActivity(intent)
+                    } catch (e: Exception) {
+                        Log.e(
+                            TAG,
+                            "Failed to initial activity to grant all files access",
+                            e
+                        )
+                        Toast.makeText(this, "没有获取sdcard的读取权限", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            builder.setCancelable(false)
+            builder.create().show()
         } else {
+            loadView()
         }
     }
 
-    private fun requestSdcardPermission() {
-        if (Build.VERSION_CODES.R <= Build.VERSION.SDK.toInt()) {
-            requestPermissionLauncher.launch(Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION))
-        } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                )
-            ) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    )
-                ) {
-                    ActivityCompat.requestPermissions(
-                        this, arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                        REQUEST_PERMISSION_CODE
-                    )
+    private fun isGranted(grantResults: IntArray): Boolean {
+        return grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION) {
+            if (isGranted(grantResults)) {
+                permissionCallbacks[STORAGE_PERMISSION]!!.onPermissionGranted()
+                permissionCallbacks[STORAGE_PERMISSION] = null
+            } else {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show()
+                permissionCallbacks[STORAGE_PERMISSION]?.let {
+                    requestStoragePermission(it, false)
                 }
             }
         }
     }
 
-    private fun showDialog(
-        activity: Activity,
-        message: String,
-        clickListener: DialogInterface.OnClickListener
-    ) {
-        AlertDialog.Builder(activity)
-            .setTitle("提示")
-            .setMessage(message)
-            .setPositiveButton(
-                "确定"
-            ) { dialog: DialogInterface?, which: Int ->
-                clickListener.onClick(
-                    dialog,
-                    which
-                )
-            }
-            .show()
+    override fun onPermissionGranted() {
+        loadView()
+    }
+
+    companion object {
+
+        private val TAG = "ChooseFile"
+
+        @JvmField
+        val PREF_TAG = "ChooseFileActivity"
+
+        @JvmField
+        val PREF_HOME = "Home"
+
+        const val PERMISSION_LENGTH = 2
+        var STORAGE_PERMISSION = 0
+        const val ALL_FILES_PERMISSION = 1
     }
 }
